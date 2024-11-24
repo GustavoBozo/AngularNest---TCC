2795e9d899d08a9bce478d25331e6a1a0193b16c
GustavoBozo <gustavinhobozo@gmail.com>
2024-11-23 22:07:14 -0300

    Login de usuario Funcionando

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NgModule } from '@angular/core';
+import { PaginaInicialComponent } from './components/pagina-inicial/pagina-inicial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'home', component: PaginaInicialComponent}
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
---
+++ frontend/src/app/components/register/register.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { Usuario } from '@prisma/client'
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+  http = inject(HttpClient)
+
+
+
+  createUser(data: {name: string, email: string, password: string}){
+    return this.http.post<Usuario>("http://localhost:3000/usuario/create", data)
+    .subscribe(usuario => {
+      console.log("Usuario Criado", usuario)
+    })
+  }
+
+
+  
+
+}
